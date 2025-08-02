3fe23674c3248c8325a5d5e659265a12edf78885
Aditya <adit10022004@gmail.com>
2025-08-02 15:38:35 +0530

    2nday commit

---
+++ app/src/main/java/com/example/tacticaledge/opponentTeam/OpponentTeam.kt
@@ -0,0 +1,103 @@
+package com.example.tacticaledge.opponentTeam
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.tacticaledge.ui.theme.greenlight
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun OpponentTeam(navController: NavController){
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(greenlight)
+    ){
+
+        Text(text = "Opponent Team",
+            color = androidx.compose.ui.graphics.Color.White,
+            fontSize = 16.sp, fontWeight =  FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 6.dp)
+            )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+label = {Text(text = "Team Name ")},
+            colors = OutlinedTextFieldDefaults.colors(
+focusedBorderColor = Color.Gray,
+                unfocusedBorderColor = Color.Gray,
+                focusedTextColor = Color.White,
+                unfocusedTextColor = Color.White
+            ),modifier = Modifier.fillMaxWidth().
+            background(greenlight),
+            shape = RoundedCornerShape(8.dp)
+
+
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Formation ",
+            color = androidx.compose.ui.graphics.Color.White,
+            fontSize = 16.sp, fontWeight =  FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 6.dp)
+        )
+
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Formation") },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Gray,
+                unfocusedBorderColor = Color.Gray,
+                focusedTextColor = Color.White,
+                unfocusedTextColor = Color.White
+            ),
+            modifier = Modifier.fillMaxWidth().
+            background(greenlight),
+            shape = RoundedCornerShape(8.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Formation ",
+            color = androidx.compose.ui.graphics.Color.White,
+            fontSize = 16.sp, fontWeight =  FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 6.dp)
+        )
+
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Formation") },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Gray,
+                unfocusedBorderColor = Color.Gray,
+                focusedTextColor = Color.White,
+                unfocusedTextColor = Color.White
+            ),
+            modifier = Modifier.fillMaxWidth().
+            background(greenlight),
+            shape = RoundedCornerShape(8.dp)
+
+        )
+    }
+
+}
